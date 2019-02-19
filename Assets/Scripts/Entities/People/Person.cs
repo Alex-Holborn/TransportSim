bdcf6c8e55c19ed97877d75972cbe12d0ba49552
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-02-19 13:26:38 +1000

    Update Person.cs

--- Assets/Scripts/Entities/People/Person.cs
+++ Assets/Scripts/Entities/People/Person.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Entities;
 using UnityEngine;
 
-public class Person : ScriptableObject
+public class Person : MovableEntity
 {
 
 }
