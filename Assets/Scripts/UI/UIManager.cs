97812ce55068d2d7316c162f4a2e64d79d2abf41
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-02-19 13:26:12 +1000

    Create UIManager.cs

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public InfoWindow OpenInfoWindow { get; private set; }
+
+    public void DisplayInfoWindow(Entity e)
+    {
+        if (OpenInfoWindow != null)
+        {
+            if (!OpenInfoWindow.Object.Equals(e))
+            {
+                Destroy(OpenInfoWindow);
+                OpenInfoWindow = ScriptableObject.CreateInstance<InfoWindow>();
+                OpenInfoWindow.setEntityObject(e);
+            }
+        }else
+        {
+            OpenInfoWindow = ScriptableObject.CreateInstance<InfoWindow>();
+            OpenInfoWindow.setEntityObject(e);
+        }
+    }
+
+}
