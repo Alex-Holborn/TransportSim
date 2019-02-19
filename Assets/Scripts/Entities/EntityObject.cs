8be7ac9dd63c4542533faa9a6977e5173dce0e83
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-02-19 13:26:45 +1000

    Create EntityObject.cs

---
+++ Assets/Scripts/Entities/EntityObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityObject : MonoBehaviour
+{
+    
+    public Entity Entity { get; private set; }
+    private UIManager manager;
+
+    public void SetEntity(Entity e)
+    {
+        Entity = e;
+    }
+    
+    private void OnMouseDown()
+    {
+        if (manager == null)
+        {
+            manager = FindObjectOfType<UIManager>();
+        }
+        manager.DisplayInfoWindow(Entity);
+    }
+}
