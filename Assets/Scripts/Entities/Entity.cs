813c73dcc671ea8029e1cdeae225ca99b283b961
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-02-19 13:26:47 +1000

    Update Entity.cs

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+public class Entity : ScriptableObject
 {
+   public EntityObject Object { get; private set; }
    
 }
