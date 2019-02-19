74c0f288b86aa1ac83f71b8fbcc69d39d6ea279c
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-02-19 13:26:17 +1000

    Create InfoWindow.cs

---
+++ Assets/Scripts/UI/InfoWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoWindow : UIComponent
+{
+    
+    public Entity Object { get; private set; }
+    
+    public void setEntityObject(Entity e)
+    {
+        Object = e;
+    }
+    
+}
