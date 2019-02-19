4f84493f4b95a932e162cd4da007c53aaef9c4e4
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-02-19 13:26:14 +1000

    Create UIComponent.cs

---
+++ Assets/Scripts/UI/UIComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIComponent : ScriptableObject
+{
+    public Canvas Canvas { get; private set; }
+
+    void Awake()
+    {
+        if (Canvas == null)
+        {
+            Canvas = FindObjectOfType<Canvas>();
+        }
+    }
+}
