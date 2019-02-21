daff742dbf878deb5f40edb642ff93112ca61b01
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-02-21 13:25:53 +1000

    Create TimeHandler.cs

---
+++ Assets/Scripts/Misc/TimeHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeHandler : MonoBehaviour
+{
+    
+    public Date CurrentDate { get; private set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentDate = new Date();
+        CurrentDate.SetDay(1);
+        CurrentDate.SetMonth(6);
+        CurrentDate.SetYear(1970);
+    }
+
+    void Update()
+    {
+        CurrentDate = Date.CreateTomorrow(CurrentDate);
+        Debug.Log("Date: " + CurrentDate.Day+"/"+CurrentDate.Month+"/"+CurrentDate.Year);
+    }
+
+}
